4474d5a7c585ad004d868edf90136a049e5db80d
Tarasov Anton <tarasov.ai@infotek.ru>
2022-06-07 18:32:19 +0300

    types, values, scope

---
+++ concepts/types.js
@@ -0,0 +1,17 @@
+/*
+    Всего 7 типов данных: null, undefined, boolean, number, string, object, symbol
+
+    False values: '', 0, null, undefined, NaN, false
+    if (false) {}
+
+    True values: [], {}, function
+    if (true) {}
+*/
+
+// '42px' - 2 = NaN
+// null + 2 = 2
+// undefined + 2 = NaN
+
+// == vs ===
+// == - делает приведение к типам
+// === - не делает, поэтому 2 != '2'
